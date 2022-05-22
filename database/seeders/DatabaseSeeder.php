719a6c4cb57b42061f4e898933a72130300b7865
Yv <raazyuvv@gmail.com>
2022-05-22 16:38:29 +0545

    Initial Commit

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Petition;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Petition::factory(50)->create();
+    }
+}
